7c5e8f8af2885b351fe3f7c3c358d143196228bf
jun <wnwjdwns1@naver.com>
2025-03-05 09:36:26 +0900

    ✨[FEAT] mealSlug, share 페이지 추가

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,26 +1,41 @@
-// import Link from "next/link";
-// import Image from "next/image";
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import MealNotFound from "../NotFound";
 
-// import classes from "./MealItem.module.css";
+export default function MealDetailPage({
+  params,
+}: {
+  params: { mealSlug: string };
+}) {
+  const meal = getMeal(params.mealSlug);
+  if (!meal) {
+    return <MealNotFound />;
+  }
 
-// export default function MealItem({ title, slug, image, summary, creator }) {
-//   return (
-//     <article className={classes.meal}>
-//       <header>
-//         <div className={classes.image}>
-//           <Image src={image} alt={title} fill />
-//         </div>
-//         <div className={classes.headerText}>
-//           <h2>{title}</h2>
-//           <p>by {creator}</p>
-//         </div>
-//       </header>
-//       <div className={classes.content}>
-//         <p className={classes.summary}>{summary}</p>
-//         <div className={classes.actions}>
-//           <Link href={`/meals/${slug}`}>View Details</Link>
-//         </div>
-//       </div>
-//     </article>
-//   );
-// }
+  meal.instructions = meal.instructions.replace(/\n/g, "<br>");
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h2>{meal.title}</h2>
+          <p className={classes.creator}>
+            by <a href={`mailTo:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
+}
