2c19b7eb9ec8e73b7b145a4ab1ea325ca2e82bc8
Jun <jun213@pm.me>
2025-03-17 13:58:42 +0900

    Revert "✨ 뉴스 필터링 기능 추가 및 뉴스 리스트 컴포넌트 통합"
    This reverts commit e0aaf4c7dd9d9f6f7567b1b125c92c684de4c2a4.

--- packages/route-guide/app/archive/@archive/[[...filter]]/page.tsx
+++ packages/route-guide/app/archive/@archive/[[...filter]]/page.tsx
@@ -1,4 +1,3 @@
-import { NewsList } from "@/app/components/news";
 import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
 import Link from "next/link";
 
@@ -8,28 +7,14 @@ export default function FilteredNewsPage({
   params: { filter: string[] };
 }) {
   const filter = params.filter;
-  const selectedYear = filter?.[0];
-  const selectedMonth = filter?.[1];
-
-  let news;
-
-  if (selectedYear && !selectedMonth) {
-    news = getNewsForYear(selectedYear);
-  }
-
-  let newsContent = <p>뉴스가 없습니다.</p>;
-
-  if (news && news.length > 0) {
-    newsContent = <NewsList news={news} />;
-  }
+  console.log(filter);
 
   const links = getAvailableNewsYears();
 
   return (
-    <>
-      <header id="archive-header">
-        <nav>
-          <ul>
+    <header id="archive-header">
+      <nav>
+        <ul>
           {links.map((link) => (
             <li key={link}>
               <Link href={`/archive/${link}`}>{link}</Link>
@@ -37,8 +22,7 @@ export default function FilteredNewsPage({
           ))}
         </ul>
       </nav>
-      </header>
-      {newsContent}
-    </>
+    </header>
+    // {/* <NewsList news={filteredNews} /> */}
   );
 }
