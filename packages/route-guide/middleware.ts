b4d050ad75336d50cacc9ce2dacc20ac0d349e24
Jun <wnwjdwns1@naver.com>
2025-03-25 09:48:50 +0900

    ✨ 미들웨어 추가

---
+++ packages/route-guide/middleware.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+import { NextRequest } from "next/server";
+
+export default function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/news",
+};
