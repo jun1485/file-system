5b65b15c20eb67d0c410d5bd5a09d66949615ab8
Jun <wnwjdwns1@naver.com>
2025-04-04 08:33:31 +0900

    ✨ Firebase 의존성 추가 및 환경 변수 설정

--- packages/wedding-invidation/next.config.js
+++ packages/wedding-invidation/next.config.js
@@ -1,14 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  cssModules: false,
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ["firebasestorage.googleapis.com"],
+    domains: ["firebasestorage.googleapis.com", "via.placeholder.com"],
     formats: ["image/avif", "image/webp"],
   },
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
+      process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID:
+      process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET:
+      process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID:
+      process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  },
+  transpilePackages: [
+    "firebase",
+    "firebase/app",
+    "firebase/firestore",
+    "firebase/storage",
+    "firebase/auth",
+  ],
 };
 
 module.exports = nextConfig;
